b2c42f27a346b143fbf6ed01468ed772f382e26d
seung-heee <tmdgmldi2595@naver.com>
2024-01-16 14:30:40 +0900

    Add 시각화 물결효과

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -2,11 +2,15 @@ import { StyleSheet, View, Text, Pressable } from 'react-native';
 
 function GoalItem(props) {
     return (
-        <Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
-            <View style={styles.goalItem}>
+        <View style={styles.goalItem}>
+            <Pressable
+                android_ripple={{ color: 'blue' }}
+                onPress={props.onDeleteItem.bind(this, props.id)}
+                style={({ pressed }) => pressed && styles.pressedItem}
+            >
                 <Text style={styles.goalText}>{props.text}</Text>
-            </View>
-        </Pressable>
+            </Pressable>
+        </View>
     )
 };
 
@@ -15,12 +19,15 @@ export default GoalItem;
 const styles = StyleSheet.create({
     goalItem: {
         margin: 5,
-        padding: 10,
         borderRadius: 10,
         backgroundColor: "skyblue",
         color: 'white',
     },
+    pressedItem: {
+        opacity: 0.5,
+    },
     goalText: {
-        color: 'white'
+        color: 'white',
+        padding: 8,
     }
 });
